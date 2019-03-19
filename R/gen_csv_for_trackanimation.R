d37e72b991a4f3b66276fae3f8e5a205d388e1b4
Martin Geissmann <martin.geissmann@gmail.com>
2019-03-19 21:01:20 +0100

    using R functions

--- R/gen_csv_for_trackanimation.R
+++ R/gen_csv_for_trackanimation.R
@@ -1,43 +1,68 @@
+# R generate CSV from our data
+
 library(tidyverse)
 library(rgdal)
 library(readxl)
 library(anytime)
 
-# read pictures
-pictures <- list.files("../img/.", include.dirs = F, recursive = F,
-                       pattern = ".png$") %>% 
-  enframe(name = "n") %>% 
-  mutate(id = str_remove(value, "W_") %>% str_remove("L.png"),
-         set = "main") %>% 
-  mutate(id = as.integer(id))
+generate_csv <- function(imagedir = "../img/.",
+                         imgprefix = "W_",
+                         imgsuffix = "L.png",
+                         csvpath = "../data/interval3.5m-gps.csv",
+                         csvdelim = " ",
+                         datecol = "X1",
+                         xcol = "X4",
+                         ycol = "X5",
+                         obscol = "X2",
+                         projection = "+init=epsg:27572",
+                         csvoutput = "../data/tomap.csv") {
+  
+  # read pictures
+  pictures <- list.files(imagedir, include.dirs = F, recursive = F,
+                         pattern = ".png$") %>% 
+    enframe(name = "n") %>% 
+    mutate(id = str_remove(value, imgprefix) %>% str_remove(imgsuffix),
+           set = "main") %>% 
+    mutate(id = as.integer(id))
+  
+  
+  
+  # read CSV location data and join with pictures (only overlapping are relevant)
+  data <- read_delim(csvpath, csvdelim, col_names = F) %>%
+    rename(Date = datecol, x = xcol, y = ycol, obs = obscol) %>% 
+    mutate(Date = Date %>% str_sub(1,10) %>% as.integer() %>% anytime()) %>% 
+    filter(obs %in% pictures$id) %>% 
+    left_join(pictures, by = c("obs" = "id"))
+  
+  orig_coords <- data %>% select(lat = x, lon = y, obs, value, set, Date)
+  
+  coordinates(orig_coords) <- c('lat', 'lon')
+  
+  # adjust projection, we have some French projection in the input location CSV
+  proj4string(orig_coords) <- CRS(projection)
+  
+  # world coordinates
+  Metric_coords <- spTransform(orig_coords, CRS("+init=epsg:4326"))
+  
+  # save as csv
+  tomap <- Metric_coords %>% 
+    as_tibble() %>% 
+    mutate(Altitude = 0, Speed = 0,
+           TimeDifference = as.double(Date - lag(Date)),
+           Distance = 0,
+           FileName = 1,
+           CodeRoute = 1) %>% 
+    select(CodeRoute, Latitude = lon, Longitude = lat, 
+           Altitude, Date, Speed, TimeDifference, Distance, 
+           FileName = obs)
+  
+  tomap %>% write.csv(csvoutput)
 
-# read CSV location data and join with pictures (only overlapping are relevant)
-data <- read_delim("../data/interval3.5m-gps.csv", " ", col_names = F) %>% 
-  mutate(Date = X1 %>% str_sub(1,10) %>% as.integer() %>% anytime(),
-         x = X4, y = X5, obs = X2) %>% 
-  filter(obs %in% pictures$id) %>% 
-  left_join(pictures, by = c("obs" = "id"))
+}
 
-orig_coords <- data %>% select(lat = x, lon = y, obs, value, set, Date)
+# if __name__ == ”__main__“
+if (!interactive()) {
+  generate_csv()
+}
 
-coordinates(orig_coords) <- c('lat', 'lon')
 
-# adjust projection, we have some French projection in the input location CSV
-proj4string(orig_coords) <- CRS("+init=epsg:27572")
-
-# world coordinates
-Metric_coords <- spTransform(orig_coords, CRS("+init=epsg:4326"))
-
-# save as csv
-tomap <- Metric_coords %>% 
-  as_tibble() %>% 
-  mutate(Altitude = 0, Speed = 0,
-         TimeDifference = as.double(Date - lag(Date)),
-         Distance = 0,
-         FileName = 1,
-         CodeRoute = 1) %>% 
-  select(CodeRoute, Latitude = lon, Longitude = lat, 
-         Altitude, Date, Speed, TimeDifference, Distance, 
-         FileName = obs)
-
-tomap %>% write.csv("../data/tomap.csv")
--- R/gen_static_maps.R
+++ R/gen_static_maps.R
@@ -1,54 +1,77 @@
+# R generate static maps with ggmap
+
 library(tidyverse)
 library(rgdal)
 library(ggmap)
 
-pictures <- list.files("../img/.", include.dirs = F, recursive = F,
-                       pattern = ".png$") %>% 
-  enframe(name = "n") %>% 
-  mutate(id = str_remove(value, "W_") %>% str_remove("L.png"),
-         set = "main") %>% 
-  mutate(id = as.integer(id))
 
-data <- read_delim("../data/interval3.5m-gps.csv", " ", col_names = F) %>% 
-  rename(x = 4, y = 5, obs = 2) %>%
-  filter(obs %in% pictures$id) %>% 
-  left_join(pictures, by = c("obs" = "id"))
-
-
-orig_coords <- data %>% select(lat = x, lon = y, obs, value, set)
-coordinates(orig_coords) <- c('lat', 'lon')
-
-proj4string(orig_coords) <- CRS("+init=epsg:27572")
-
-Metric_coords <- spTransform(orig_coords, CRS("+init=epsg:4326"))
-
-tomap <- Metric_coords %>% 
-  as_tibble() %>% 
-  select(obs, Longitude = lat, Latitude = lon, value, set) %>% 
-  mutate(col = if_else(set == "main", "blue", "red"))
-
-# get static map from Google
-if(!file.exists("api.key")) { 
-  stop("you need a Google Map API key and put it in a file called api.key") }
-
-#key <- read_file("api.key")
-key <- readChar("api.key", file.info("api.key")$size - 1)
-register_google(key)
-
-mapcenter <- tomap %>% summarise(lon = mean(Longitude),
-                                 lat = mean(Latitude)) %>% 
-  unlist()
-
-statmap <- get_map(location = mapcenter, 
-                   zoom = 16)
-
-
-for (i in 1:nrow(tomap)) {
-  plot <- ggmap(statmap, extent = "device") + 
-    geom_point(data = tomap[i,], aes(x = Longitude, y = Latitude),
-               size = 6, color = "red")
-
-  ggsave(paste0("../staticmap/map_", tomap[i, "value"]), plot)
+generate_csv <- function(imagedir = "../img/.",
+                         imgprefix = "W_",
+                         imgsuffix = "L.png",
+                         csvpath = "../data/interval3.5m-gps.csv",
+                         csvdelim = " ",
+                         datecol = "X1",
+                         xcol = "X4",
+                         ycol = "X5",
+                         obscol = "X2",
+                         projection = "+init=epsg:27572",
+                         mapsoutput = "../staticmap/map_") {
+  
+  pictures <- list.files(imagedir, include.dirs = F, recursive = F,
+                         pattern = ".png$") %>% 
+    enframe(name = "n") %>% 
+    mutate(id = str_remove(value, imgprefix) %>% str_remove(imgsuffix),
+           set = "main") %>% 
+    mutate(id = as.integer(id))
+  
+  data <- read_delim(csvpath, csvdelim, col_names = F) %>%
+    rename(Date = datecol, x = xcol, y = ycol, obs = obscol) %>% 
+    mutate(Date = Date %>% str_sub(1,10) %>% as.integer() %>% anytime()) %>% 
+    filter(obs %in% pictures$id) %>% 
+    left_join(pictures, by = c("obs" = "id"))
+  
+  
+  orig_coords <- data %>% select(lat = x, lon = y, obs, value, set)
+  coordinates(orig_coords) <- c('lat', 'lon')
+  
+  proj4string(orig_coords) <- CRS(projection)
+  
+  Metric_coords <- spTransform(orig_coords, CRS("+init=epsg:4326"))
+  
+  tomap <- Metric_coords %>% 
+    as_tibble() %>% 
+    select(obs, Longitude = lat, Latitude = lon, value, set) %>% 
+    mutate(col = if_else(set == "main", "blue", "red"))
+  
+  # get static map from Google
+  if(!file.exists("api.key")) { 
+    stop("you need a Google Map API key and put it in a file called api.key") }
+  
+  #key <- read_file("api.key")
+  key <- readChar("api.key", file.info("api.key")$size - 1)
+  register_google(key)
+  
+  mapcenter <- tomap %>% summarise(lon = mean(Longitude),
+                                   lat = mean(Latitude)) %>% 
+    unlist()
+  
+  statmap <- get_map(location = mapcenter, 
+                     zoom = 16)
+  
+  
+  for (i in 1:nrow(tomap)) {
+    plot <- ggmap(statmap, extent = "device") + 
+      geom_point(data = tomap[i,], aes(x = Longitude, y = Latitude),
+                 size = 6, color = "red")
+    
+    ggsave(paste0(mapsoutput, tomap[i, "value"]), plot)
+  }
+  
+  print("static map images should be in the folder")
+  
 }
 
-print("static map images should be in the folder")
+# if __name__ == ”__main__“
+if (!interactive()) {
+  generate_csv()
+}
